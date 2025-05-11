acd19b83e66ae7c787968f93ddc25d0dafc424ff
ihorborys <ihorborys@gmail.com>
2025-05-11 12:29:05 +0300

    Edited ErrorMessage

--- src/api/api.js
+++ src/api/api.js
@@ -3,11 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com";
 
 export const getImages = async (searchQuery) => {
-  // console.log("🔍 searchQuery:", searchQuery);
-  //
-  // if (!searchQuery || searchQuery.trim() === "") {
-  //   throw new Error("Search query is missing");
-  // }
   const { data } = await axios.get("/search/photos", {
     params: {
       client_id: "QMyvt8jb24108khvQVvUu6GbAKmLMegr2UZ9NZ7VatM",
